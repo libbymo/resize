ad4ad2445931fb21abafd3e80559daad04a8b166
Libby Molina <lmolina@elctech.com>
2012-02-28 12:37:08 -0800

    Re-evaluating code, adding notes via DONT_README for code cleanup.

--- resize-translation.coffee
+++ resize-translation.coffee
@@ -1,3 +1,29 @@
 $ = jQuery
+
+# this is gross. 
+window.$parent
+window.days = 131
+window.dayWidth
+window.handleWidth = 0
+window.$tooltip
+  
 $.fn.resizeMe  = (options) ->
   settings = $.extend {}, options
+  
+  @each ->
+    $parent = $(@)
+    addTooltip();
+  
+    $('.rContainer', this).each ->
+      console.log 'in rContainer each'
+      $container = $(@)
+      $container.prepend('<div class="lHandle"></div><div class="rHandle"></div>')
+      rHandleBind($container);
+      lHandleBind($container);
+      
+addTooltip = -> 
+  $('body').append('<div class="rtooltip"></div>')
+    
+rHandleBind = ( $container ) ->
+  
+lHandleBind = ( $container ) ->
--- resize-translation.js
+++ resize-translation.js
@@ -1,11 +1,42 @@
 (function() {
-  var $;
+  var $, addTooltip, lHandleBind, rHandleBind;
 
   $ = jQuery;
 
+  window.$parent;
+
+  window.days = 131;
+
+  window.dayWidth;
+
+  window.handleWidth = 0;
+
+  window.$tooltip;
+
   $.fn.resizeMe = function(options) {
     var settings;
-    return settings = $.extend({}, options);
+    settings = $.extend({}, options);
+    return this.each(function() {
+      var $parent;
+      $parent = $(this);
+      addTooltip();
+      return $('.rContainer', this).each(function() {
+        var $container;
+        console.log('in rContainer each');
+        $container = $(this);
+        $container.prepend('<div class="lHandle"></div><div class="rHandle"></div>');
+        rHandleBind($container);
+        return lHandleBind($container);
+      });
+    });
   };
+
+  addTooltip = function() {
+    return $('body').append('<div class="rtooltip"></div>');
+  };
+
+  rHandleBind = function($container) {};
+
+  lHandleBind = function($container) {};
 
 }).call(this);
